2070445dc7685fe0fcf8527fa7a28a9c9e438517
Akshin Goswami <akshin.goswami@gmail.com>
2020-01-03 11:51:19 +1100

    New Macros Added

--- DS_CLR/Test_Macro.h
+++ DS_CLR/Test_Macro.h
@@ -10,15 +10,20 @@
 #pragma once
 
 // Enable UI Error Input
-#define UI_INPUR_ERROR_CHECK	0
+#define UI_INPUR_ERROR_CHECK	1
 
 // Enable Helper Function Tests
+// Enable all tests selected
+#define TEST_ENABLE				0
+// Select tests to enable below
 #define TEST_OCV				0
 #define TEST_LIST_FILES			0
 #define TEST_PREPROC			0
 #define TEST_DRAW_CONTOURS		0
-#define TEST_DRAW_DETECT_PREDIC	1
-
+#define TEST_DRAW_DETECT_PREDIC	0
 // Enable Droplet Analysis Tests
 #define TEST_TIME_VECTOR		0
 #define TEST_DROPLET_ANALYSIS	0
+
+// enable the main loop function
+#define MAIN_ENABLE				1
