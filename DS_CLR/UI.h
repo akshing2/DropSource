eaa7f05fc3c935d3ca873b1934273b30154e8214
Akshin Goswami <akshin.goswami@gmail.com>
2020-05-11 15:53:26 +1000

    Included new form validation function

--- DS_CLR/UI.h
+++ DS_CLR/UI.h
@@ -67,6 +67,83 @@ namespace UI_ERROR
 		return ret;
 	}
 
+	// Function to determine if std::string is a valid number
+    int valid_number(std::string str)
+    {
+        int i = 0, j = str.length() - 1;
+
+        // Handling whitespaces 
+        while (i < str.length() && str[i] == ' ')
+            i++;
+        while (j >= 0 && str[j] == ' ')
+            j--;
+
+        if (i > j)
+            return false;
+
+        // if string is of length 1 and the only 
+        // character is not a digit 
+        if (i == j && !(str[i] >= '0' && str[i] <= '9'))
+            return false;
+
+        // If the 1st char is not '+', '-', '.' or digit 
+        if (str[i] != '.' && str[i] != '+'
+            && str[i] != '-' && !(str[i] >= '0' && str[i] <= '9'))
+            return false;
+
+        // To check if a '.' or 'e' is found in given 
+        // string. We use this flag to make sure that 
+        // either of them appear only once. 
+        bool flagDotOrE = false;
+
+        for (i; i <= j; i++) {
+            // If any of the char does not belong to 
+            // {digit, +, -, ., e} 
+            if (str[i] != 'e' && str[i] != '.'
+                && str[i] != '+' && str[i] != '-'
+                && !(str[i] >= '0' && str[i] <= '9'))
+                return false;
+
+            if (str[i] == '.') {
+                // checks if the char 'e' has already 
+                // occurred before '.' If yes, return 0. 
+                if (flagDotOrE == true)
+                    return false;
+
+                // If '.' is the last character. 
+                if (i + 1 > str.length())
+                    return false;
+
+                // if '.' is not followed by a digit. 
+                if (!(str[i + 1] >= '0' && str[i + 1] <= '9'))
+                    return false;
+            }
+
+            else if (str[i] == 'e') {
+                // set flagDotOrE = 1 when e is encountered. 
+                flagDotOrE = true;
+
+                // if there is no digit before 'e'. 
+                if (!(str[i - 1] >= '0' && str[i - 1] <= '9'))
+                    return false;
+
+                // If 'e' is the last Character 
+                if (i + 1 > str.length())
+                    return false;
+
+                // if e is not followed either by 
+                // '+', '-' or a digit 
+                if (str[i + 1] != '+' && str[i + 1] != '-'
+                    && (str[i + 1] >= '0' && str[i] <= '9'))
+                    return false;
+            }
+        }
+
+        /* If the string skips all above cases, then
+        it is numeric*/
+        return true;
+    }
+
 	std::string SYS2std_string(System::String^ conv)
 	{
 		msclr::interop::marshal_context context;
